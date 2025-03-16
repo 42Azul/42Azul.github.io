21279f28f081cfc3920bc00528c67d1c7094a1ad
42Azul <fernando30102000@gmail.com>
2025-03-16 21:31:03 +0100

    Added github actions workflow to create a github pages with the portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const SCRIPT = process.env.npm_lifecycle_script || "";
+
+const LIVE_URL = "https://42Azul.github.io";
+
+const isBuild = SCRIPT.includes("astro build");
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: isBuild ? LIVE_URL : LOCALHOST_URL,
+  server: {
+    port: SERVER_PORT,
+  },
 });
